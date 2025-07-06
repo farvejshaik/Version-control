c8fa00d3a024fad6562de93bd0cf993108ab7a52
Farvej Farru <farvejfaru128@gmail.com>
2025-07-06 11:42:45 +0530

    till-HomePage

--- frontend/src/components/Repos.jsx
+++ frontend/src/components/Repos.jsx
@@ -1,13 +1,21 @@
 import Repo from "./Repo";
 
-const Repos = () => {
+const Repos = ({ repos, loading, alwaysFullWidth = false }) => {
+  const className = alwaysFullWidth ? "w-full" : "lg:w-2/3 w-full";
+
   return (
-    <div className={`lg:w-2/3 w-full bg-glass rounded-lg px-8 py-6`}>
-      <ol className="relative border-s border-gray-200">
-        <Repo />
-        <Repo />
-        <Repo />
-      </ol>
+    <div className={`${className} bg-glass rounded-lg px-8 py-6`}>
+      {loading ? (
+        <p className="text-center text-gray-500">Loading...</p>
+      ) : repos.length > 0 ? (
+        <ol className="relative border-s border-gray-200">
+          {repos.map((repo) => (
+            <Repo key={repo.id} repo={repo} />
+          ))}
+        </ol>
+      ) : (
+        <p className="flex items-center justify-center h-32">No Repos Found</p>
+      )}
     </div>
   );
 };
